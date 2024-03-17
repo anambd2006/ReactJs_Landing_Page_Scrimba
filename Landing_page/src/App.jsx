a437caba197a7367bf51645107d31115ad31b139
anam <anambd2006@gmail.com>
2024-03-17 19:11:40 +0600

    first commit

---
+++ Landing_page/src/App.jsx
@@ -0,0 +1,18 @@
+
+import MainComponent from "./component/MainComponent"
+import Navbars from "./component/Navbars"
+
+
+function App() {
+  
+    return (
+        <div>
+            <Navbars />
+            <MainComponent />
+            
+        </div>
+    )
+       
+}
+
+export default App
